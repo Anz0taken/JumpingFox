be86502a41a86fa2589ed02a5346f376ce215a3e
Luca Gargiulo <lucgar@10-20-56-220.0200.fed.it.iosda.org>
2023-12-15 11:13:15 +0100

    Pen spawn, fix and bugs

--- GameTest/GameScene.swift
+++ GameTest/GameScene.swift
@@ -8,8 +8,6 @@
 import SpriteKit
 import GameplayKit
 
-
-
 class GameScene: SKScene, SKPhysicsContactDelegate {
 
     var background = SKSpriteNode(imageNamed: "sky")
@@ -24,6 +22,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var onAir : Bool = false
     let playerCategory: UInt32 = 0x1 << 0
     let groundCategory: UInt32 = 0x1 << 1
+    let penCategory: UInt32 = 0x1 << 2
+
     
     let terrainHeight = 38
     
@@ -34,6 +34,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     let IS_JUMP_CHUNK = 1
     
     var terrainYOffset = 100;
+    
+    var generatedTerrainNodes: [SKSpriteNode] = []
 
     override func didMove(to view: SKView) {
             
@@ -85,11 +87,27 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     func didBegin(_ contact: SKPhysicsContact) {
+        let contactMask = contact.bodyA.categoryBitMask | contact.bodyB.categoryBitMask
+
+        if contactMask == (playerCategory | penCategory) {
+            if contact.bodyA.categoryBitMask == penCategory {
+                // Remove the pen from the scene
+                if let penNode = contact.bodyA.node as? SKSpriteNode {
+                    penNode.removeFromParent()
+                }
+            } else if contact.bodyB.categoryBitMask == penCategory {
+                // Remove the pen from the scene
+                if let penNode = contact.bodyB.node as? SKSpriteNode {
+                    penNode.removeFromParent()
+                }
+            }
+        }
     }
+
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let playerPhysicsBody = playerFigure.physicsBody {
-            // Check if the player is on the ground
+            
             if playerPhysicsBody.velocity.dy == 0 {
                 jumpSound.run(SKAction.play())
                 let jumpForce: CGFloat = 30.0
@@ -111,16 +129,13 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var frameCounter: Int = 0
     override func update(_ currentTime: TimeInterval) {
-        // Increment the frame counter
         frameCounter += 1
 
-        // Change the fox figure every 10 frames
         if frameCounter % 50 == 0 {
-            // Animate the fox
+
             let animationAction = SKAction.animate(with: foxTextures, timePerFrame: 0.1)
             playerFigure.run(animationAction)
 
-            // Reset the frame counter
             frameCounter = 0
         }
 
@@ -132,6 +147,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         if Int(playerFigure.position.x + frame.width/2) > lastXPixel {
             generateRandomTerrain(isStartTerrain: false)
+            deallocateUnusedTerrain()
         }
         
         if playerFigure.position.y < -500 {
@@ -147,8 +163,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                 view!.presentScene(gameOverScene)
             
             }
-                    
-            
         }
     }
    
@@ -183,23 +197,43 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                 let terrain = createTerrain(imageNamed: randomType, width: randomWidth, height: terrainHeight)
                 positionAndAddTerrain(terrain, yOffset: terrainYOffset)
                 positionAndAddUnderground(lastWidth: randomWidth, yOffset: terrainYOffset)
-
-                if Int(arc4random_uniform(UInt32(10))) == 1 {
+                generatedTerrainNodes.append(terrain)
+                
+                if Int(arc4random_uniform(UInt32(4))) == 1 {
                     spawnTree(at: CGPoint(x: lastXPixel - 20, y: Int(-frame.size.height)/2 + 206), yOffset: terrainYOffset)
                 }
             }
         }
+
+        guard let terrainAboveToSpawn = getRandomTerrainFromLastNum(lastElements: 20) else {
+            print("Not enough terrain nodes available.")
+            return
+        }
+
+        spawnCollectibleItem(x: terrainAboveToSpawn.position.x, y: CGFloat(Int(terrainAboveToSpawn.position.y) + terrainHeight + 20), nameImage: "pen")
+    }
+    
+    func getRandomTerrainFromLastNum(lastElements: Int) -> SKSpriteNode? {
+        guard generatedTerrainNodes.count >= lastElements else {
+            // Ensure there are at least 20 elements in the array
+            return nil
+        }
+
+        let startIndex = generatedTerrainNodes.count - lastElements
+        let randomIndex = Int(arc4random_uniform(UInt32(lastElements)))
+        return generatedTerrainNodes[startIndex + randomIndex]
     }
     
     func positionAndAddUnderground(lastWidth: Int, yOffset: Int) {
         let terrainHeight = 64
-        var backgroundTerrainX = lastXPixel - (lastWidth) / 2
+        let backgroundTerrainX = lastXPixel - (lastWidth) / 2
         var actualY = yOffset + 38
-        var terrainName = (lastWidth == 32) ? "underground1" : "underground2"
+        let terrainName = (lastWidth == 32) ? "underground1" : "underground2"
 
         while actualY > -64 {
             let terrain = createTerrain(imageNamed: terrainName, width: lastWidth, height: terrainHeight)
             terrain.position = CGPoint(x: backgroundTerrainX, y: Int(-frame.size.height) / 2 + actualY)
+            generatedTerrainNodes.append(terrain)
             self.addChild(terrain)
             actualY -= terrainHeight;
         }
@@ -241,6 +275,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     func positionAndAddTerrain(_ terrain: SKSpriteNode, yOffset: Int) {
         let randomX = lastXPixel + Int(terrain.size.width) / 2
         terrain.position = CGPoint(x: randomX, y: Int(-frame.size.height)/2 + 80 + yOffset)
+        generatedTerrainNodes.append(terrain)
         lastXPixel = randomX + Int(terrain.size.width) / 2
         self.addChild(terrain)
     }
@@ -281,8 +316,25 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         tree.zPosition = 0
         tree.color = treeColor
         tree.colorBlendFactor = 1.0
+        generatedTerrainNodes.append(tree)
 
         self.addChild(tree)
+    }
+    
+    func spawnCollectibleItem(x: CGFloat, y: CGFloat, nameImage: String) {
+        let collectibleItem = SKSpriteNode(imageNamed: nameImage)
+        collectibleItem.size = CGSize(width: 30, height: 30)
+        collectibleItem.position = CGPoint(x: x, y: y)
+
+        collectibleItem.physicsBody = SKPhysicsBody(rectangleOf: collectibleItem.size)
+        collectibleItem.physicsBody?.isDynamic = false
+        collectibleItem.physicsBody?.categoryBitMask = penCategory // Set a unique category for the pen
+        collectibleItem.physicsBody?.contactTestBitMask = playerCategory // Set the category that will contact with the pen
+        collectibleItem.physicsBody?.collisionBitMask = 0 // No collision with anything
+
+        collectibleItem.zPosition = 1
+
+        self.addChild(collectibleItem)
     }
 
     func generateRandomTreeColor() -> UIColor {
@@ -295,7 +347,22 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         return UIColor(red: red, green: green, blue: blue, alpha: 1.0)
     }
 
-    
+    func deallocateUnusedTerrain() {
+        let minXPixelToKeep = Int(playerFigure.position.x) - Int(frame.size.width) * 2 // Change the factor as needed
+
+        // Filter out the terrain nodes that are too far behind
+        var nodesToEliminate = generatedTerrainNodes.filter { node in
+            let nodeMinX = Int(playerFigure.position.x) -  Int(node.position.x)
+            return nodeMinX > 800
+        }
+
+        // Remove the deallocated nodes from the scene
+        for node in nodesToEliminate {
+            if node.parent != nil {
+                node.removeFromParent()
+            }
+        }
+    }
     
 //    func aggiungiPunteggio(_ nuovoPunteggio: Int) {
 //        var miglioriPunteggi = UserDefaults.standard.array(forKey: "MiglioriPunteggi") as? [Int] ?? []
